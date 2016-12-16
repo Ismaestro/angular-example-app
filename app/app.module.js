44c5910a72b02c5c619486295017a320acfc6f10
Ismael Ramos <ramossilvanismael@gmail.com>
2016-12-16 11:41:29 +0100

    chore(files): removed comments and reformat code

--- app/app.module.js
+++ app/app.module.js
@@ -31,9 +31,4 @@ var AppModule = (function () {
     return AppModule;
 }());
 exports.AppModule = AppModule;
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/ 
 //# sourceMappingURL=app.module.js.map
--- app/app.module.ts
+++ app/app.module.ts
@@ -15,10 +15,3 @@ import { AppComponent }  from './app.component';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
