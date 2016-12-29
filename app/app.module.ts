6acc0133dcaf756d4cb0290ee01f4ab48103d589
Ismael Ramos <ramossilvanismael@gmail.com>
2016-12-29 17:10:31 +0100

    chore(http): added http module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
