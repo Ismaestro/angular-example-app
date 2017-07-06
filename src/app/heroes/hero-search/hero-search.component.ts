586b1d6c63a0c43422ce478b439d203c791b9003
Ismael Ramos <ramossilvanismael@gmail.com>
2017-07-06 09:04:48 +0200

    fix(autocomplete): solved problem with form control and array model

--- src/app/heroes/hero-search/hero-search.component.ts
+++ src/app/heroes/hero-search/hero-search.component.ts
@@ -1,12 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {Router} from '@angular/router';
-
 import {APP_CONFIG} from '../../config/app.config';
 import {IAppConfig} from '../../config/iapp.config';
 import {LoggerService} from '../../core/logger.service';
-
 import {Hero} from '../shared/hero.model';
-
 import {FormControl} from '@angular/forms';
 import {HeroService} from '../shared/hero.service';
 
@@ -19,45 +16,35 @@ import {HeroService} from '../shared/hero.service';
   ]
 })
 
-export class HeroSearchComponent implements OnInit {
+export class HeroSearchComponent {
   heroes: Array<Hero> = [];
-  searchTerms;
-  showDropDown;
-  searchBox;
-
-  stateCtrl: FormControl;
-  filteredStates: any;
-
-  filterStates(val: string) {
-    return val ? this.heroes.filter(s => s.name.toLowerCase().indexOf(val.toLowerCase()) === 0)
-      : this.heroes;
-  }
+  heroFormControl: FormControl;
+  filteredHeroes: any;
 
   constructor(@Inject(APP_CONFIG) private appConfig: IAppConfig,
               private heroService: HeroService,
               private router: Router) {
-    this.showDropDown = false;
+    this.heroFormControl = new FormControl();
 
-    this.stateCtrl = new FormControl();
-    this.filteredStates = this.stateCtrl.valueChanges
-      .startWith(null)
-      .map(name => this.filterStates(name));
-  }
-
-  ngOnInit(): void {
     this.heroService.getHeroes().then((heroes) => {
-      for (const hero of heroes) {
-
-      }
       this.heroes = heroes;
+
+      this.heroFormControl.valueChanges
+        .startWith(null)
+        .map(value => this.filterHeroes(value))
+        .subscribe(heroesFiltered => {
+          this.filteredHeroes = heroesFiltered;
+        });
     });
   }
 
+  filterHeroes(val: string) {
+    return val ? this.heroes.filter(hero => hero.name.toLowerCase().indexOf(val.toLowerCase()) === 0)
+      : this.heroes;
+  }
+
   gotoDetail(hero: Hero): void {
-    let link = ['/' + this.appConfig.routes.heroes, hero.id];
-    this.router.navigate(link);
-    this.showDropDown = false;
-    this.searchBox = null;
+    this.router.navigate(['/' + this.appConfig.routes.heroes, hero.id]);
     LoggerService.log('Moved to hero with id: ' + hero.id);
   }
 }
