09819b5091d273ba0697ebfe4875225b7f93b9ef
ismaestro <ramossilvanismael@gmail.com>
2017-08-13 20:23:37 +0200

    feat(tests): added constructor test for nav component

--- src/app/core/nav/nav.component.spec.ts
+++ src/app/core/nav/nav.component.spec.ts
@@ -1,7 +1,7 @@
 import {async, TestBed} from '@angular/core/testing';
 import {NavComponent} from './nav.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {TranslateModule} from '@ngx-translate/core';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 import {APP_CONFIG, AppConfig} from '../../config/app.config';
 import {HeroService} from '../../heroes/shared/hero.service';
 import {MaterialModule} from '../../shared/modules/material.module';
@@ -34,6 +34,12 @@ describe('NavComponent', () => {
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.debugElement.componentInstance;
     progressBarService = TestBed.get(ProgressBarService);
+  }));
+
+  it('should create nav component with constructor', (() => {
+    const translateService = TestBed.get(TranslateService);
+    const instance = new NavComponent(AppConfig, progressBarService, translateService);
+    expect(instance).toBeTruthy();
   }));
 
   it('should create nav component', (() => {
