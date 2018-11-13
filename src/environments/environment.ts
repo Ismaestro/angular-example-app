7e69b8cab00e0396b4232bd2571ba98c423f21e9
Ismael Ramos <ramossilvanismael@gmail.com>
2018-11-13 23:21:34 +0100

    Update firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDy8PobaeiM8GvE0ds-bWkzdw2iDo0CZtQ',
-    authDomain: 'angularexampleapp.firebaseapp.com',
-    databaseURL: 'https://angularexampleapp.firebaseio.com',
-    projectId: 'angularexampleapp',
-    storageBucket: 'angularexampleapp.appspot.com',
-    messagingSenderId: '456717340071'
+    apiKey: 'AIzaSyC4RNNlqXSwTM1Kt6Pbgutq2yuavSQ9WnU',
+    authDomain: 'ismaestro-angularexampleapp.firebaseapp.com',
+    databaseURL: 'https://ismaestro-angularexampleapp.firebaseio.com',
+    projectId: 'ismaestro-angularexampleapp',
+    storageBucket: 'ismaestro-angularexampleapp.appspot.com',
+    messagingSenderId: '965114235515'
   }
 };
