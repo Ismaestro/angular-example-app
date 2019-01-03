36324f8c6c93529be0f6d646271bf0e349d90312
infuse <infuse89@gmail.com>
2019-01-03 11:43:14 +0200

    fix

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export filename="extension-v-$(cat manifest-prod.json | sed 's/.*"version": "\(.*\)".*/\1/;t;d')-prod.zip"
 aws s3 cp ${filename} s3://${S3_BUCKET}
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"<@Alexander Ivanov> https://${S3_BUCKET}.s3.amazonaws.com/$filename\"}" ${webhookURL}
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"<@Alexander Ivanov> https://${S3_BUCKET}.s3.amazonaws.com/${filename}\"}" ${webhookURL}
