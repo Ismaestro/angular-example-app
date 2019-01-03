3f8f38ae988ca89ecb8229dff21020fd581d8c9f
infuse <infuse89@gmail.com>
2019-01-03 12:04:45 +0200

    fix

--- exec.sh
+++ exec.sh
@@ -3,4 +3,4 @@ export version=$(cat manifest-prod.json | sed 's/.*"version": "\(.*\)".*/\1/;t;d
 export filename="extension-v-${version}-prod.zip"
 aws s3 cp ${filename} s3://${S3_BUCKET}
 export url = $(aws s3 presign ${S3_BUCKET}/${filename} --expires-in 15552000)
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"<@Alexander Ivanov> Extension ${version} was builded successfully [<'${S3_BUCKET}'|Download>]\"}" ${webhookURL}
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"<@Alexander Ivanov> Extension *${version}* was builded successfully [<${url}|Download>]\"}" ${webhookURL}
