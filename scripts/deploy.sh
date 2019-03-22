76439e2b2dd0326e74b61cb008164f548f8fd524
ismaestro <ramossilvanismael@gmail.com>
2019-03-22 19:03:00 +0100

    Fix deploy script to rename every language index to index2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,8 @@ rm -Rf public &&
 mkdir public &&
 cp -a dist/browser/. public/ &&
 mv public/assets/common/* public/ &&
-rm -Rf public/es/assets/common &&
-mv public/index.html public/app.html &&
+rm -Rf public/assets/common public/es/assets/common &&
+mv public/index.html public/index2.html &&
+mv public/es/index.html public/es/index2.html &&
 cp -a dist/ functions/dist &&
 firebase deploy
