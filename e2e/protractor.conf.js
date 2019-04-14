2239bbca3e92dd290a91e0ebdf4e1b84270b23f7
ismaestro <ramossilvanismael@gmail.com>
2019-04-14 21:14:32 +0200

    Fix protractor config

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,9 +9,6 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    chromeOptions: {
-      args: [ "--headless" ]
-    },
     'browserName': 'chrome'
   },
   directConnect: true,
