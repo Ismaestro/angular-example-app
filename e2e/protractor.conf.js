b91351911719b08de7685217216546552dbf8d80
ismaestro <ramossilvanismael@gmail.com>
2019-04-15 10:15:18 +0200

    Fix fade in animation

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,6 +9,9 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
+    chromeOptions: {
+      args: [ "--headless" ]
+    },
     'browserName': 'chrome'
   },
   directConnect: true,
