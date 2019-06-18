a1373130c92544d22cbb7a07f4510a59a71b953a
Ismael Ramos <ramos.silvan.ismael@gmail.com>
2019-06-18 11:08:58 +0200

    feat(karma): update reporters

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'mocha'],
