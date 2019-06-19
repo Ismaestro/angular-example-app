5a213ae6ab3693b875f5b9dd9fb73b3f13872e58
Ismael Ramos <ramos.silvan.ismael@gmail.com>
2019-06-19 14:57:04 +0200

    feat(travis): update firebase deploy commands 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,4 @@ mv public/assets/common/* public/ &&
 rm -Rf public/assets/common public/es/assets/common &&
 mv public/index.html public/index2.html &&
 mv public/es/index.html public/es/index2.html &&
-cp -a dist/ functions/dist &&
-firebase deploy
+cp -a dist/ functions/dist
