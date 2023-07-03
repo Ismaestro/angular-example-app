8699866d2253846f1b3c266e206c6fa29732a20b
Ismael Ramos <ismael.ramos@meta.es>
2023-07-03 20:55:46 +0200

    feat(flyio): change server prod value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  graphqlHost: 'https://nestjs-example-app.onrender.com/',
+  graphqlHost: 'https://fly.io/apps/nestjs-example-app/',
   domain: 'https://angular-example-app.onrender.com',
 };
