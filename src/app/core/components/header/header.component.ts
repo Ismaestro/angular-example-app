dceb971e43f4583b28b6363d216a3b1625d71270
ismaestro <ramossilvanismael@gmail.com>
2024-12-16 00:51:20 +0100

    refactor(core): rewrite header component logic

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -5,12 +5,10 @@ import {
   effect,
   inject,
 } from '@angular/core';
-import { POKEMON_URLS, ROOT_URLS } from '~core/constants/urls.constants';
+import { AUTH_URLS, POKEMON_URLS, ROOT_URLS, USER_URLS } from '~core/constants/urls.constants';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { FirstTitleCasePipe } from '~core/pipes/first-title-case.pipe';
 import { NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
-import type { NavItem } from '~core/components/header/header.constants';
-import { HEADER_NAV_ITEMS, NavItemId, NavItemType } from '~core/components/header/header.constants';
 import { PokemonSearchComponent } from '~features/pokemon-detail/components/pokemon-search/pokemon-search.component';
 import { AuthenticationService } from '~features/authentication/services/authentication.service';
 import type { Pokemon } from '~features/pokemon-detail/types/pokemon.type';
@@ -19,6 +17,9 @@ import { LanguageSelectorComponent } from '~core/components/language-selector/la
 
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
+import '@shoelace-style/shoelace/dist/components/menu/menu.js';
+import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
 
 @Component({
   selector: 'app-header',
@@ -38,14 +39,14 @@ import '@shoelace-style/shoelace/dist/components/icon/icon.js';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeaderComponent {
-  private readonly router = inject(Router);
   private readonly authenticationService = inject(AuthenticationService);
   private readonly themeManagerService = inject(ThemeManagerService);
 
+  readonly router = inject(Router);
   readonly Theme = Theme;
   readonly ROOT_URLS = ROOT_URLS;
-  readonly navItemType = NavItemType;
-  readonly navItems: NavItem[] = HEADER_NAV_ITEMS;
+  readonly AUTH_URLS = AUTH_URLS;
+  readonly USER_URLS = USER_URLS;
 
   isUserLoggedIn = this.authenticationService.isUserLoggedIn();
   menuOpen = false;
@@ -58,22 +59,15 @@ export class HeaderComponent {
     effect(() => {
       this.isUserLoggedIn = this.authenticationService.isUserLoggedIn();
     });
-
-    this.findNavItem(NavItemId.LOGOUT)!.click = this.logoutUser.bind(this);
-    this.findNavItem(NavItemId.SEARCH)!.change = this.loadPokemonLink.bind(this);
   }
 
-  private findNavItem(id: NavItemId) {
-    return this.navItems.find((item) => item.id === id);
-  }
-
-  private logoutUser() {
+  logoutUser() {
     this.pokemonLoaded = undefined;
     this.authenticationService.logOut();
     void this.router.navigate([ROOT_URLS.home]);
   }
 
-  private loadPokemonLink(event: unknown) {
+  loadPokemonLink(event: unknown) {
     this.pokemonLoaded = event as Pokemon;
     const pokemonName = this.pokemonLoaded.name;
     if (pokemonName) {
