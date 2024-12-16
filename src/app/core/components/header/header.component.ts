eaa3ac359c7b0476e73fbcbd8519244866ee1813
ismaestro <ramossilvanismael@gmail.com>
2024-12-16 10:08:09 +0100

    refactor(core): move theme button to a component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -12,8 +12,8 @@ import { NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
 import { PokemonSearchComponent } from '~features/pokemon-detail/components/pokemon-search/pokemon-search.component';
 import { AuthenticationService } from '~features/authentication/services/authentication.service';
 import type { Pokemon } from '~features/pokemon-detail/types/pokemon.type';
-import { Theme, ThemeManagerService } from '~core/services/theme-manager.service';
 import { LanguageSelectorComponent } from '~core/components/language-selector/language-selector.component';
+import { ThemeButtonComponent } from '~core/components/theme-button/theme-button.component';
 
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
@@ -35,15 +35,14 @@ import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
     NgOptimizedImage,
     LanguageSelectorComponent,
     NgTemplateOutlet,
+    ThemeButtonComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeaderComponent {
   private readonly authenticationService = inject(AuthenticationService);
-  private readonly themeManagerService = inject(ThemeManagerService);
 
   readonly router = inject(Router);
-  readonly Theme = Theme;
   readonly ROOT_URLS = ROOT_URLS;
   readonly AUTH_URLS = AUTH_URLS;
   readonly USER_URLS = USER_URLS;
@@ -53,7 +52,6 @@ export class HeaderComponent {
   pokemonLoaded: Pokemon | undefined;
   pokemonLoading = false;
   pokemonLoadedRoute = '';
-  themeSelected = this.themeManagerService.getThemeFromLocalStorageValue();
 
   constructor() {
     effect(() => {
@@ -77,15 +75,6 @@ export class HeaderComponent {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
-  }
-
-  toggleTheme() {
-    this.themeSelected =
-      this.themeManagerService.getThemeFromLocalStorageValue() === Theme.DARK ||
-      this.themeSelected === null
-        ? Theme.LIGHT
-        : Theme.DARK;
-    this.themeManagerService.setTheme(this.themeSelected);
   }
 
   closeMenu() {
