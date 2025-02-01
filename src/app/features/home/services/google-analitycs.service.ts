71ffee40a3f87d0bc133edd4bdfaed15ea5809ba
ismaestro <ramossilvanismael@gmail.com>
2025-02-01 07:30:28 +0100

    refactor(home): add use of environment api base url variable

--- src/app/features/home/services/google-analitycs.service.ts
+++ src/app/features/home/services/google-analitycs.service.ts
@@ -1,15 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import type { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '~environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoogleAnalyticsService {
-  private readonly apiUrl = 'https://nestjs-example-app.fly.dev/v1/analytics/realtime-users';
   private readonly httpClient = inject(HttpClient);
+  private readonly apiUrl = environment.apiBaseUrl;
 
   getRealtimeUsers(): Observable<{ activeUsers: number }> {
-    return this.httpClient.get<{ activeUsers: number }>(this.apiUrl);
+    const getRealtimeUsersEndpoint = `${this.apiUrl}/v1/analytics/realtime-users`;
+    return this.httpClient.get<{ activeUsers: number }>(getRealtimeUsersEndpoint);
   }
 }
