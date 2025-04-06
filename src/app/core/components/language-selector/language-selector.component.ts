1d20094f4daab3f05830b66da4622807c736c464
ismaestro <ramossilvanismael@gmail.com>
2025-04-06 21:27:07 +0200

    refactor(app): move language selector to signals v2

--- src/app/core/components/language-selector/language-selector.component.ts
+++ src/app/core/components/language-selector/language-selector.component.ts
@@ -1,12 +1,15 @@
-import type { OnInit } from '@angular/core';
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+  signal,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 import { LanguageService } from '~core/services/language.service';
 
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
-import { AUTH_URLS } from '~core/constants/urls.constants';
-import { translations } from '../../../../locale/translations';
 
 @Component({
   selector: 'app-language-selector',
@@ -15,16 +18,9 @@ import { translations } from '../../../../locale/translations';
   imports: [UpperCasePipe],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class LanguageSelectorComponent implements OnInit {
+export class LanguageSelectorComponent {
+  private readonly languageService = inject(LanguageService);
+
   readonly router = inject(Router);
-  readonly languageService = inject(LanguageService);
-
-  localeIdText = '';
-
-  ngOnInit() {
-    this.localeIdText = this.languageService.convertLocaleToAcceptLanguage();
-  }
-
-  protected readonly AUTH_URLS = AUTH_URLS;
-  protected readonly translations = translations;
+  readonly localeIdText = signal(this.languageService.convertLocaleToAcceptLanguage());
 }
