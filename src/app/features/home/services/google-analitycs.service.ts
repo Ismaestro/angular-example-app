9edbb5775f301334924be1210a0b48702f8111b9
ismaestro <ramossilvanismael@gmail.com>
2025-04-10 00:26:16 +0200

    refactor(app): remove unused method in google analytics service

--- src/app/features/home/services/google-analitycs.service.ts
+++ src/app/features/home/services/google-analitycs.service.ts
@@ -1,19 +1,13 @@
-import { inject, Injectable } from '@angular/core';
-import type { Observable } from 'rxjs';
-import { HttpClient, httpResource, type HttpResourceRef } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { httpResource, type HttpResourceRef } from '@angular/common/http';
 import { environment } from '~environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoogleAnalyticsService {
-  private readonly httpClient = inject(HttpClient);
   private readonly apiUrl = environment.apiBaseUrl;
 
-  getRealtimeUsers(): Observable<{ activeUsers: number }> {
-    const getRealtimeUsersEndpoint = `${this.apiUrl}/v1/analytics/realtime-users`;
-    return this.httpClient.get<{ activeUsers: number }>(getRealtimeUsersEndpoint);
-  }
   getRealtimeUsersResource(): HttpResourceRef<{ activeUsers: number }> {
     return httpResource<{ activeUsers: number }>(`${this.apiUrl}/v1/analytics/realtime-users`, {
       defaultValue: { activeUsers: 1 },
