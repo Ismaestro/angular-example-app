5cc1f9e447311bc0e4140cad7f2353291a7ae078
ismaestro <ramossilvanismael@gmail.com>
2025-11-06 18:54:10 +0100

    feat(tools): remove commit lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['build', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test']],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'repo',
+        'tools',
+        'styles',
+        'e2e',
+        'version',
+        'app',
+        'core',
+        'auth',
+        'home',
+        'pokemon',
+        'pokedex',
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never'],
+  },
 };
