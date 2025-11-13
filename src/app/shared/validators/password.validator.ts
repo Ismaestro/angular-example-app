a2c75b6047260a4f39458007fb062e9fbe170df9
ismaestro <ramossilvanismael@gmail.com>
2025-11-13 13:36:07 +0100

    chore(app): refactor `passwordValidator` to simplify logic and enhance readability

--- src/app/shared/validators/password.validator.ts
+++ src/app/shared/validators/password.validator.ts
@@ -1,18 +1,18 @@
 import type { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function passwordValidator(): ValidatorFn {
-  const validators = [
-    (value: string) => /[A-Z]/u.test(value), // Has uppercase
-    (value: string) => /[a-z]/u.test(value), // Has lowercase
-    (value: string) => /[0-9]/u.test(value), // Has numeric
-    (value: string) => value.length >= 8, // Is valid length
-  ];
-
   return (control: AbstractControl): ValidationErrors | null => {
-    const value = control.value as string;
-    if (!value) {
-      return null;
-    }
-    return validators.every((function_) => function_(value)) ? null : { passwordStrength: true };
+    const value = (control.value ?? '') as string;
+    if (!value) return null;
+    const isValid = isStrongPassword(value);
+    return isValid ? null : { passwordStrength: true };
   };
+}
+
+function isStrongPassword(value: string) {
+  const hasUppercase = /[A-Z]/u.test(value);
+  const hasLowercase = /[a-z]/u.test(value);
+  const hasNumber = /\d/u.test(value);
+  const hasMinLength = value.length >= 8;
+  return hasUppercase && hasLowercase && hasNumber && hasMinLength;
 }
