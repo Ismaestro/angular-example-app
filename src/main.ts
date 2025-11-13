fd23db6a8564ec2e74d8b5361adac5f6891f96fe
ismaestro <ramossilvanismael@gmail.com>
2025-11-13 01:59:49 +0100

    chore(repo): remove unused angular localize reference in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-/// <reference types="@angular/localize" />
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
